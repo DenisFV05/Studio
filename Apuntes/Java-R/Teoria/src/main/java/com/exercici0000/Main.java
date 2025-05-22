e13143f89a23be2b34956d07ce783f09a2cc6b79
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2025-05-22 16:19:51 +0200

    voy a hacer la teoria de 0 esta semana
    de facil a dificil

--- Apuntes/Java-R/Teoria/src/main/java/com/exercici0000/Main.java
+++ Apuntes/Java-R/Teoria/src/main/java/com/exercici0000/Main.java
@@ -2,7 +2,8 @@ package com.exercici0000;
 
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Hola Java!");
+        System.out.println("Hola tio me llamo pepe ááááá");
         /* TODO: 
            Fes aquí el codi de l'exercici
         */
